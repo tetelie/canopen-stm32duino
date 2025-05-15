41ec4466d44d5a28506c8bfeb14c701aa68e6bd7
Iban <ibanleginyora@gmail.com>
2025-05-15 04:47:56 +0200

    Update master_rsp.py

--- main/master_rsp.py
+++ main/master_rsp.py
@@ -2,7 +2,7 @@ import can
 import canopen
 
 # Ouvre l'interface CAN (SocketCAN)
-bus = can.interface.Bus(channel='can0', bustype='socketcan')
+#bus = can.interface.Bus(channel='can0', bustype='socketcan')
 
 # Crée un client CANopen (esclave nœud 0)
 network = canopen.Network()
@@ -10,7 +10,8 @@ network.connect(bustype='socketcan', channel='can0', bitrate=500000)
 
 # Charge un Object Dictionary (exemple minimal, tu peux utiliser un vrai EDS file)
 # Ici on crée un objet manuel, sans EDS
-node = canopen.RemoteNode(0, 'stm32.eds')  # idéalement avec un fichier EDS réel
+node = canopen.RemoteNode(0, 'stm32.eds')
+node.id = 0
 network.add_node(node)
 
 # Demande de lecture SDO sur 0x2000:0
@@ -19,6 +20,6 @@ try:
     print(f"Valeur lue à 0x2000:0 = {value}")
 except canopen.SdoAbortedError as e:
     print(f"Erreur SDO: {e}")
-
-# Ferme la connexion CAN
-network.disconnect()
+finally:
+	# Ferme la connexion CAN
+	network.disconnect()
