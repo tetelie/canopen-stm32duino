c5850e5429b096c0b93cf3779e2002a6c8a19ac7
Iban <ibanleginyora@gmail.com>
2025-05-15 06:52:27 +0200

    Update

--- main/test/test.py
+++ main/test/test.py
@@ -6,7 +6,9 @@ network = canopen.Network()
 
 network.connect(channel='can0', bustype='socketcan', bitrate=500000)
 
-node = network.add_node(0, 'sample.eds')
+node = network.add_node(0, 'stm32.eds')
 
+for node_id in network:
+	print("node = ", network[node_id])
 
 network.disconnect()
