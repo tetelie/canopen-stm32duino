9eaf2af1974aa7943a003f5cb20b704d06006553
Σlie * <44349276+tetelie@users.noreply.github.com>
2025-05-21 10:28:15 +0200

    add debug function

--- arduino-ide/vacop/vacop.ino
+++ arduino-ide/vacop/vacop.ino
@@ -6,18 +6,63 @@
 #include "hal_conf_extra.h"
 #include <STM32_CAN.h>
 
+#define DEBUG 1
+
 
 
 CO_NMT_control_t nmt_control;    // il semblerait y avoir un problème à verifier lors des tests
 
 
 STM32_CAN Can1(CAN1, DEF); // Broches PA11/PA12 pour CAN1
-CO_t *CO = NULL;
+CO_t *CO = NULL;           // Objet CANopen
 
 CAN_message_t latestMsg;
 volatile bool newMessage = false;
 bool messagePending = false;
 CAN_message_t msg;
+
+
+/*
+  debug function
+  description: print with or without jump 'message' if DEBUG Enabled
+    - message : char *
+    - ln : bool (default true) // jump line
+  return type : void
+*/
+void debug(bool ln = true) {
+  if (DEBUG) {
+    if (ln) Serial.println();
+  }
+}
+
+// Pour les types numériques avec base
+template <typename T>
+typename std::enable_if<std::is_integral<T>::value, void>::type
+debug(T message, int format, bool ln = true) {
+    if(DEBUG){
+        if (ln) {
+            Serial.println(message, format);
+        } else {
+            Serial.print(message, format);
+        }
+    }
+}
+
+// Pour les types génériques
+template <typename T>
+void debug(T message, bool ln = true) {
+    if(DEBUG){
+        if (ln) {
+            Serial.println(message);
+        } else {
+            Serial.print(message);
+        }
+    }
+}
+
+
+
+
 
 // === Callback appelée à la réception d’un message CANopen configuré ===
 void myRxCallback(void *object, void *msgPtr) {
@@ -34,27 +79,27 @@ void setup() {
     CO_config_t config;
     OD_INIT_CONFIG(config);
 
-    Serial.println("début initialisation");
+    debug("début initialisation");
     CO = CO_new(&config, NULL);
     if (CO == NULL) {
-        Serial.println("Erreur d'allocation CO");
+        debug("Erreur d'allocation CO");
         while (1);
     }
 
     CO_CANmodule_disable(CO->CANmodule);
 
-    Serial.println("après co_news");
+    debug("après co_news");
 
     uint32_t errInfo = 0;
 
     // Init module CANopen avec STM32_CAN
     CO_ReturnError_t err = CO_CANinit(CO, (void*)&Can1, 500000 /*bitrate*/);
     if (err != CO_ERROR_NO) {
-        Serial.println("Erreur d'init CO_CANinit");
+        debug("Erreur d'init CO_CANinit");
         while (1);
     }
 
-    Serial.println("après init");
+    debug("après init");
     delay(5000);
 
     // Init objets CANopen (PDO, SDO, NMT, etc.)itRa
@@ -73,39 +118,39 @@ void setup() {
         &errInfo               // erreur détaillée en cas d’échec
     );
     if (err != CO_ERROR_NO) {
-        Serial.println("Erreur d'init CO_CANopenInit");
+        debug("Erreur d'init CO_CANopenInit");
         while (1);
     }
 
-    Serial.println("après Open_init");
+    debug("après Open_init");
     delay(5000);
 
 
 
     err = CO_CANopenInitPDO(CO, CO->em, OD, 0x01, &errInfo);
 
-    Serial.println("après InitPDO");
+    debug("après InitPDO");
     delay(5000);
 
 
     // Exemple d’enregistrement d’un callback sur l’ID 0x180 (TPDO1 d’un autre noeud)
     CO_CANrxBufferInit(CO->CANmodule, 0, 0x180, 0x7FF, false, NULL, myRxCallback);
 
-    Serial.println("après rxBufferInit");
+    debug("après rxBufferInit");
     delay(5000);
 
-    
 
-    Serial.println("CANopenNode prêt");
+
+    debug("CANopenNode prêt");
 }
 
 void loop() {
-    //Serial.println("paul est beau");
+    //debug("paul est beau");
     // Tick CANopenNode
     uint32_t now = millis();
     CO_process(CO,true, now, NULL);
 
-    
+
     if (Can1.read(msg)) {
         messagePending = true; // ← là on injecte dans CANopen
     }
@@ -115,16 +160,16 @@ void loop() {
     if (newMessage) {
         newMessage = false;
 
-        Serial.print("Reçu ID: 0x");
-        Serial.print(latestMsg.id, HEX);
-        Serial.print(" DLC: ");
-        Serial.print(latestMsg.len);
-        Serial.print(" Data: ");
+        debug("Reçu ID: 0x", false);
+        debug(latestMsg.id, HEX, false);
+        debug(" DLC: ", false);
+        debug(latestMsg.len, false);
+        debug(" Data: ", false);
         for (uint8_t i = 0; i < latestMsg.len; i++) {
-            Serial.print(latestMsg.buf[i], HEX);
-            Serial.print(" ");
+            debug(latestMsg.buf[i], HEX, false);
+            debug();
         }
-        Serial.println();
+        debug();
     }
 
     delay(1);
